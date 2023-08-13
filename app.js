fe3c28181c6c035f269d9ff76548c780fcac04ee
devarjankars <devarjankars@gmail.com>
2023-08-13 15:29:53 +0530

    body parser

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 
 const express= require('express')
 const app=express();
-
+const bodyparser=require('body-parser')
 app.listen(3030);
-const user=[{id:1, name:"sanjay"}, {id:2, name:"riya"},{id:3, name:"prachi"}];
-app.get('/',(req,res)=>{
-    
-    res.send('<h1>sanjay</h1>');
+
+app.use(bodyparser.urlencoded({extended:false}))
+app.get('/add-item',(req,res,next)=>{
+    console.log("add item page ");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>')
+})
+app.post('/product',(req,res,next)=>{
+
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res,next) =>{
+console.log("The Middelever Fun");
+res.send('<h1>Welcome to Node js</h1>')
 })
